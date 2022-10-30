4d05f61a66fa979795f96045f9d1b2b669724caf
sunlumos <735287480@qq.com>
2022-10-30 11:54:38 +0800

    添加代码解释

--- lesson1.py
+++ lesson1.py
@@ -1,23 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# 构建训练数据集 x,y分别对应输入和输入，相同的索引对应相同的输入和输出
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
  
- #
+# 定义目标模型  y = x * w
 def forward(x):
     return x*w
  
- 
+# 定义误差函数
 def loss(x, y):
     y_pred = forward(x)
     return (y_pred - y)**2
  
  
 # 穷举法
+# 定义权重的list，存放权重
 w_list = []
+# 定义MSE的list，用于存放权重对应的误差值
 mse_list = []
-for w in np.arange(0.0, 4.1, 0.1):
+for w in np.arange(0.0, 4.1, 0.1): #设置间隔为0.1，取0.0,0.1……到4.0之间的数组作为w
     print("w=", w)
     l_sum = 0
     for x_val, y_val in zip(x_data, y_data):
@@ -29,6 +32,7 @@ for w in np.arange(0.0, 4.1, 0.1):
     w_list.append(w)
     mse_list.append(l_sum/3)
     
+# 使用plt绘制图形
 plt.plot(w_list,mse_list)
 plt.ylabel('Loss')
 plt.xlabel('w')
